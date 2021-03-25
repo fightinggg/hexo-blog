77e8958aad5a83f62753e3b4048ca50523786afb
hellowang <hellowang@example.com>
2021-03-25 19:11:05 +0800

    -

--- themes/next/source/js/nightAuto.js
+++ themes/next/source/js/nightAuto.js
@@ -8,7 +8,7 @@ autoScheme = () => {
     if (last != hour) {
         localStorage.setItem('lastAutoUpdateScheme', hour)
 
-        if (hour < 6 || hour > 19) { // 如果是晚上
+        if (hour <= 6 || hour >= 19) { // 如果是晚上
             if (!document.documentElement.classList.contains('darkScheme')) {
                 layer.msg('晚上好呀！为您切换到夜间模式！', { offset: 't', anim: 6 });
                 localStorage.setItem('Scheme', 'Dark');
@@ -16,7 +16,7 @@ autoScheme = () => {
             }
         } else { // 如果是白天
             if (document.documentElement.classList.contains('darkScheme')) {
-                layer.msg('晚上好呀！为您切换到白天模式！', { offset: 't', anim: 6 });
+                layer.msg('白天好呀！为您切换到白天模式！', { offset: 't', anim: 6 });
                 localStorage.removeItem('Scheme');
                 document.documentElement.classList.toggle('darkScheme');
             }
