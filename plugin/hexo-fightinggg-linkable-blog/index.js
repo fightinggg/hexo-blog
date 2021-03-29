c9d73f2aa9a28e3b588fc02b71bd9dcba3e54407
wsx <246553278@qq.com>
2021-03-29 17:08:54 +0800

    -

---
+++ plugin/hexo-fightinggg-linkable-blog/index.js
@@ -0,0 +1,16 @@
+x = 1
+
+regMapConfig = hexo.config.linkable || []
+regMap = regMapConfig.map(o => ({
+    reg: new RegExp("(" + o.key + ")", "gi"),
+    replace: "[$1](" + o.url + ")"
+}))
+
+hexo.extend.filter.register('before_post_render', function (data) {
+    if (data.layout === 'post') {
+        regMap.forEach(o => {
+            data.content = data.content.replace(o.reg, o.replace);
+        })
+    }
+    return data;
+});
---
+++ source/_posts/NET/PING的时候发生了什么.md
@@ -0,0 +1,21 @@
+---
+date: 2021-03-29 11:14:14
+updated: 2021-03-29 11:14:14
+mathjax: true
+typora-root-url: ..\..
+---
+
+# PING
+
+我们常常使用ping来测试网络的联通性，但是你知道他是属于哪一次层的吗？其实ping属于网络层，他使用ICMP协议, 他没有使用UDP或者TCP
+
+<!-- more -->
+
+ICMP报文是被IP报文包裹的。 ![image-20210329114420000](/images/image-20210329114420000.png)
+
+
+
+# 参考
+
+[解析ping原理及实现](https://www.jianshu.com/p/14113212cd18)
+
