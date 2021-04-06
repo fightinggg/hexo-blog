b0959a8b5a40831df728ed3b9095106aa64fbbbd
wsx <246553278@qq.com>
2021-04-06 17:21:41 +0800

    background img

--- source/js/changeBackground.js
+++ source/js/changeBackground.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var img = new Image();
-    img.src = '/images/background/snow.jpg';
-
-    changeBackground = () => {
-        document.body.style.backgroundImage = `url(/images/background/snow.jpg)`;
-        msgUtils.showSuccessMsg('背景图片画质太低了，为您换一张更高的', 3000)
-    }
 
     changeImg = () => {
-        if (document.documentElement.className === "") {
-            changeBackground();
-        } else {
-            // msgUtils.showSuccessMsg("等待下一次换背景")
-            setTimeout(changeImg, 3000)
+        var img = new Image();
+        img.src = '/images/background/snow.jpg';
+
+        changeBackground = () => {
+            document.body.style.backgroundImage = `url(/images/background/snow.jpg)`;
+            // msgUtils.showSuccessMsg('背景图片画质太低了，为您换一张更高的', 3000)
+        }
+
+        changeImg = () => {
+            if (document.documentElement.className === "") {
+                changeBackground();
+            } else {
+                // msgUtils.showSuccessMsg("等待下一次换背景")
+                setTimeout(changeImg, 3000)
+            }
+        }
+
+        img.onload = () => {
+            changeImg()
         }
     }
 
-    img.onload = () => {
-        changeImg()
-    }
+    setTimeout(changeImg, 10000);
+
 })
