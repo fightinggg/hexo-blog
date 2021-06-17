e889c1858a3823b01f3776268ac40be935066c88
wsx <246553278@qq.com>
2021-06-18 03:32:09 +0800

    rock back

--- my-app/src/AfterBlogCard.js
+++
@@ -1,8 +0,0 @@
-import Comments from "./Comments"
-import { Card } from "antd"
-
-export default function AfterBlogCard(props) {
-    return <>
-        <Card><Comments path={props.path} /></Card>
-    </>
-}
