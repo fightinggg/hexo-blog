b55475302231c565bd150c3f9e9661df05187337
fightinggg <246553278@qq.com>
2021-10-18 15:31:56 +0800

    -

---
+++ .SUMMARY.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path")
+
+function readFileList(dir, filesList = []) {
+    const files = fs.readdirSync(dir);
+
+
+    files.forEach((item, index) => {
+        if(item.startsWith(".")){
+            return
+        }
+
+        var fullPath = path.join(dir, item);
+        const stat = fs.statSync(fullPath);
+        
+        if (stat.isDirectory()) {  
+            filesList.push(dir+"/index.md");
+            readFileList(path.join(dir, item), filesList);
+        } else {
+            if(fullPath.endsWith(".md")){
+                filesList.push(fullPath);
+            }          
+        }
+    });
+    return filesList;
+}
+
+filesList = []
+readFileList('.',filesList)
+filesList = Array.from(new Set(filesList))
+
+filesList.forEach(name=>{
+    const url = name.split('/')
+    var str = ''
+    for(i=2;i<url.length;i++){
+        str += '    '
+    }
+    console.log('%s* [%s](%s)',str,url[url.length-2],name)
+})
+
+//  node .SUMMARY.js > SUMMARY.md 
